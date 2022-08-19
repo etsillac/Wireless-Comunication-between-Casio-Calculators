10841b784560b7b45fa043b75d42c0084820b0dd
Etsillac <callistegabriel0@gmail.com>
2022-08-19 21:40:34 +0200

    Ready to do some reaaly work on the project

--- Casio Application/FXSH_Build.bat
+++ Casio Application/FXSH_Build.bat
@@ -2,7 +2,7 @@
 rem Do not edit! This batch file is created by CASIO fx-9860G SDK.
 
 if exist debug\*.obj  del debug\*.obj
-if exist WCMP.G1A  del WCMP.G1A
+if exist WMCP.G1A  del WMCP.G1A
 
 cd debug
 if exist FXADDINror.bin  del FXADDINror.bin
@@ -11,7 +11,7 @@ cd ..
 if not exist debug\FXADDINror.bin  goto error
 
 "C:\Users\calli\fx-9860G SDK\Tools\MakeAddinHeader363.exe" "C:\Users\calli\Documents\Github\Wireless-Comunication-between-Casio-Calculators\Casio Application"
-if not exist WCMP.G1A  goto error
+if not exist WMCP.G1A  goto error
 echo Build has completed.
 goto end
 
